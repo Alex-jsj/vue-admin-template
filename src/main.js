a345a573ea1a7f76a81096a093c7490d9cf57f5e
陈泽勇 <chenzeyongjsj@163.com>
2019-04-01 18:48:03 +0800

    update

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ import vuescroll from 'vuescroll/dist/vuescroll-native';
 import 'vuescroll/dist/vuescroll.css';
 Vue.use(vuescroll);
 
-// 第一个是videoJs的样式，后一个是vue-video-player的样式，因为考虑到我其他业务组件可能也会用到视频播放，所以就放在了main.js
+// 第一个是videoJs的样式，后一个是vue-video-player的样式，考虑到其他业务组件可能也会用到视频播放，所以放在main.js
 require('video.js/dist/video-js.css');
 require('vue-video-player/src/custom-theme.css');
 //把VueVideoPlayer导入并挂在到vue上
