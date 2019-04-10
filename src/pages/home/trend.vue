402953e3fd9d090f620f96914eb20945ea43d473
alex <chenzeyongjsj@163.com>
2019-04-11 03:12:56 +0800

    remove keep-alive

--- src/pages/home/trend.vue
+++ src/pages/home/trend.vue
@@ -1,18 +1,18 @@
 <template>
-	<div id="trend">
-		<div class="title-box">
-			<span class="title float-left">{{$t('home.trend')}}</span>
-			<router-link to="/pages/board" target="_blank" class="board">
-				<el-button type>{{$t('home.dataBoard')}}</el-button>
-			</router-link>
-			<p class="date">{{$t('home.QueryTime')}}：{{start_date}} {{$t('base.to')}} {{end_date}}</p>
-		</div>
-		<!-- content -->
-		<div class="echarts">
-			<span class="no-data" v-show="!data[0]">{{$t('base.noData')}}</span>
-			<div id="echart-bar" :class="{'canShow':data[0]}"></div>
-		</div>
-	</div>
+    <div id="trend">
+        <div class="title-box">
+            <span class="title float-left">{{$t('home.trend')}}</span>
+            <router-link to="/pages/board" target="_blank" class="board">
+                <el-button type>{{$t('home.dataBoard')}}</el-button>
+            </router-link>
+            <p class="date">{{$t('home.QueryTime')}}：{{start_date}} {{$t('base.to')}} {{end_date}}</p>
+        </div>
+        <!-- content -->
+        <div class="echarts">
+            <span class="no-data" v-show="!data[0]">{{$t('base.noData')}}</span>
+            <div id="echart-bar" :class="{'canShow':data[0]}"></div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -24,238 +24,238 @@ require("echarts/lib/chart/line");
 require("echarts/lib/component/tooltip");
 require("echarts/lib/component/legend");
 export default {
-	name: "trend",
-	data() {
-		return {
-			// 查询时间
-			start_date: "",
-			end_date: ""
-		};
-	},
-	props: {
-		data: Array
-	},
-	watch: {
-		listenMenuCollapse() {
-			// 动态变化canvas宽度
-			setTimeout(function() {
-				echarts.init(document.getElementById("echart-bar")).resize();
-			}, 500);
-		},
-		listenContentWidthType() {
-			// 动态变化canvas宽度
-			setTimeout(function() {
-				echarts.init(document.getElementById("echart-bar")).resize();
-			}, 500);
-		},
-		listenMenuType() {
-			// 动态变化canvas宽度
-			setTimeout(function() {
-				echarts.init(document.getElementById("echart-bar")).resize();
-			}, 500);
-		},
-		peoples_data_title() {
-			let chart = echarts.init(document.getElementById("echart-bar"));
-			chart.clear();
-			this.the_chart();
-		}
-	},
-	computed: {
-		// 菜单是否收起
-		listenMenuCollapse() {
-			return this.$store.state.menuCollapse;
-		},
-		// 菜单模式
-		listenMenuType() {
-			return this.$store.state.menuType;
-		},
-		// 页面宽度
-		listenContentWidthType() {
-			return this.$store.state.contentWidthType;
-		},
-		peoples_data_title() {
-			return {
-				data1: this.$t("home.canvas.data1"),
-				data2: this.$t("home.canvas.data2"),
-				data3: this.$t("home.canvas.data3")
-			};
-		}
-	},
-	created: function() {
-		// 报考趋势时间范围(当前月份)
-		this.scope();
-	},
-	mounted() {
-		// init
-		this.$nextTick(() => {
-			this.data[0] && this.the_chart();
-		});
-	},
-	methods: {
-		// 趋势echart
-		the_chart: function() {
-			// 基于准备好的dom，初始化echarts实例
-			let myChart = echarts.init(document.getElementById("echart-bar"));
-			const that = this;
-			let data1 = [];
-			let data2 = [];
-			let data3 = [];
-			let dates = [];
-			let list = JSON.parse(JSON.stringify(that.data));
-			list.map(item => {
-				data1.push(item.data1);
-				data2.push(item.data2);
-				data3.push(item.data3);
-				dates.push(item.day);
-			});
-			// 图表配置
-			myChart.setOption({
-				tooltip: {
-					trigger: "axis"
-				},
-				legend: {
-					data: [
-						this.peoples_data_title.data1,
-						this.peoples_data_title.data2,
-						this.peoples_data_title.data3
-					]
-				},
-				grid: {
-					left: "1%",
-					right: "1%",
-					bottom: "3%",
-					containLabel: true
-				},
-				toolbox: {
-					feature: {
-						saveAsImage: {}
-					}
-				},
-				xAxis: {
-					type: "category",
-					// boundaryGap: false,
-					axisTick: {
-						show: false
-					},
-					data: dates
-				},
-				yAxis: {
-					type: "value",
-					axisTick: {
-						show: false
-					},
-					splitLine: {
-						lineStyle: {
-							type: "dotted"
-						}
-					}
-				},
-				series: [
-					{
-						name: this.peoples_data_title.data1,
-						type: "line",
-						symbolSize: 7, //拐点圆的大小
-						data: data1,
-						smooth: 0.3, // 折线弧度
-						itemStyle: {
-							normal: {
-								color: "#4771FF",
-								label: {
-									show: true,
-									position: "top"
-								}
-							}
-						}
-					},
-					{
-						name: this.peoples_data_title.data2,
-						type: "line",
-						symbolSize: 7, //拐点圆的大小
-						data: data2,
-						smooth: 0.3, // 折线弧度
-						itemStyle: {
-							normal: {
-								color: "#f3566f",
-								label: {
-									show: true,
-									position: "top"
-								}
-							}
-						}
-					},
-					{
-						name: this.peoples_data_title.data3,
-						type: "line",
-						symbolSize: 7, //拐点圆的大小
-						data: data3,
-						smooth: 0.3, // 折线弧度
-						itemStyle: {
-							normal: {
-								color: "#E6A23C",
-								label: {
-									show: true,
-									position: "top"
-								}
-							}
-						}
-					}
-				]
-			});
-			var timer = null;
-			window.addEventListener("resize", function() {
-				// 防抖处理
-				if (timer) {
-					clearTimeout(timer);
-				}
-				timer = setTimeout(function() {
-					myChart.resize();
-				}, 200);
-			});
-		},
-		// 报考趋势时间范围(当前月份)
-		scope: function() {
-			let year = new Date().getFullYear();
-			let month = new Date().getMonth() + 1;
-			month < 10 ? (month = "0" + month) : month;
-			let day = new Date().getDate();
-			day < 10 ? (day = "0" + day) : day;
-			// 赋值
-			this.start_date = `${year}-${month}-01`;
-			this.end_date = `${year}-${month}-${day}`;
-		}
-	}
+    name: "trend",
+    data() {
+        return {
+            // 查询时间
+            start_date: "",
+            end_date: ""
+        };
+    },
+    props: {
+        data: Array
+    },
+    watch: {
+        listenMenuCollapse() {
+            // 动态变化canvas宽度
+            setTimeout(function() {
+                echarts.init(document.getElementById("echart-bar")).resize();
+            }, 500);
+        },
+        listenContentWidthType() {
+            // 动态变化canvas宽度
+            setTimeout(function() {
+                echarts.init(document.getElementById("echart-bar")).resize();
+            }, 500);
+        },
+        listenMenuType() {
+            // 动态变化canvas宽度
+            setTimeout(function() {
+                echarts.init(document.getElementById("echart-bar")).resize();
+            }, 500);
+        },
+        trend_title() {
+            let chart = echarts.init(document.getElementById("echart-bar"));
+            chart.clear();
+            this.the_chart();
+        }
+    },
+    computed: {
+        // 菜单是否收起
+        listenMenuCollapse() {
+            return this.$store.state.menuCollapse;
+        },
+        // 菜单模式
+        listenMenuType() {
+            return this.$store.state.menuType;
+        },
+        // 页面宽度
+        listenContentWidthType() {
+            return this.$store.state.contentWidthType;
+        },
+        trend_title() {
+            return {
+                data1: this.$t("home.canvas.data1"),
+                data2: this.$t("home.canvas.data2"),
+                data3: this.$t("home.canvas.data3")
+            };
+        }
+    },
+    created: function() {
+        // 报考趋势时间范围(当前月份)
+        this.scope();
+    },
+    mounted() {
+        // init
+        this.$nextTick(() => {
+            this.data[0] && this.the_chart();
+        });
+    },
+    methods: {
+        // 趋势echart
+        the_chart: function() {
+            // 基于准备好的dom，初始化echarts实例
+            let myChart = echarts.init(document.getElementById("echart-bar"));
+            const that = this;
+            let data1 = [];
+            let data2 = [];
+            let data3 = [];
+            let dates = [];
+            let list = JSON.parse(JSON.stringify(that.data));
+            list.map(item => {
+                data1.push(item.data1);
+                data2.push(item.data2);
+                data3.push(item.data3);
+                dates.push(item.day);
+            });
+            // 图表配置
+            myChart.setOption({
+                tooltip: {
+                    trigger: "axis"
+                },
+                legend: {
+                    data: [
+                        this.trend_title.data1,
+                        this.trend_title.data2,
+                        this.trend_title.data3
+                    ]
+                },
+                grid: {
+                    left: "1%",
+                    right: "1%",
+                    bottom: "3%",
+                    containLabel: true
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                xAxis: {
+                    type: "category",
+                    // boundaryGap: false,
+                    axisTick: {
+                        show: false
+                    },
+                    data: dates
+                },
+                yAxis: {
+                    type: "value",
+                    axisTick: {
+                        show: false
+                    },
+                    splitLine: {
+                        lineStyle: {
+                            type: "dotted"
+                        }
+                    }
+                },
+                series: [
+                    {
+                        name: this.trend_title.data1,
+                        type: "line",
+                        symbolSize: 7, //拐点圆的大小
+                        data: data1,
+                        smooth: 0.3, // 折线弧度
+                        itemStyle: {
+                            normal: {
+                                color: "#4771FF",
+                                label: {
+                                    show: true,
+                                    position: "top"
+                                }
+                            }
+                        }
+                    },
+                    {
+                        name: this.trend_title.data2,
+                        type: "line",
+                        symbolSize: 7, //拐点圆的大小
+                        data: data2,
+                        smooth: 0.3, // 折线弧度
+                        itemStyle: {
+                            normal: {
+                                color: "#f3566f",
+                                label: {
+                                    show: true,
+                                    position: "top"
+                                }
+                            }
+                        }
+                    },
+                    {
+                        name: this.trend_title.data3,
+                        type: "line",
+                        symbolSize: 7, //拐点圆的大小
+                        data: data3,
+                        smooth: 0.3, // 折线弧度
+                        itemStyle: {
+                            normal: {
+                                color: "#E6A23C",
+                                label: {
+                                    show: true,
+                                    position: "top"
+                                }
+                            }
+                        }
+                    }
+                ]
+            });
+            var timer = null;
+            window.addEventListener("resize", function() {
+                // 防抖处理
+                if (timer) {
+                    clearTimeout(timer);
+                }
+                timer = setTimeout(function() {
+                    myChart.resize();
+                }, 200);
+            });
+        },
+        // 报考趋势时间范围(当前月份)
+        scope: function() {
+            let year = new Date().getFullYear();
+            let month = new Date().getMonth() + 1;
+            month < 10 ? (month = "0" + month) : month;
+            let day = new Date().getDate();
+            day < 10 ? (day = "0" + day) : day;
+            // 赋值
+            this.start_date = `${year}-${month}-01`;
+            this.end_date = `${year}-${month}-${day}`;
+        }
+    }
 };
 </script>
 
 <style lang="less" scoped>
 @import "~assets/css/mixin.less";
 #trend {
-	width: 100%;
-	height: 400px;
-	border: 1px solid #edeef1;
-	margin-top: 0.875rem;
-	background: #fff;
-	.echarts {
-		width: 100%;
-		height: calc(~"100% - 40px - 10px");
-		position: relative;
-		margin-top: 10px;
-		.no-data {
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			font-size: 12px;
-			color: @grey;
-		}
-		#echart-bar {
-			width: 100%;
-			height: 350px;
-			opacity: 0;
-			&.canShow {
-				opacity: 1;
-			}
-		}
-	}
+    width: 100%;
+    height: 400px;
+    border: 1px solid #edeef1;
+    margin-top: 0.875rem;
+    background: #fff;
+    .echarts {
+        width: 100%;
+        height: calc(~"100% - 40px - 10px");
+        position: relative;
+        margin-top: 10px;
+        .no-data {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 12px;
+            color: @grey;
+        }
+        #echart-bar {
+            width: 100%;
+            height: 350px;
+            opacity: 0;
+            &.canShow {
+                opacity: 1;
+            }
+        }
+    }
 }
 </style>
--- src/pages/public/index.vue
+++ src/pages/public/index.vue
@@ -1,27 +1,27 @@
 <template>
-	<div class="wrap" :class="{'collapse-wrap':isCollapse,'isFixed':menuFixed,'menuType-2':menuType === 2,'content-width-type-2':contentWidthType==='Fixed'}">
-		<!-- 顶部信息条 -->
-		<topBar/>
-		<!-- 侧边菜单 -->
-		<sideBar/>
-		<!-- 右-部分 -->
-		<div class="right-wrap">
-			<!-- 面包屑 -->
-			<crumb/>
-			<!-- 多标签 -->
-			<tags-view/>
-			<!-- 页面容器 -->
-			<transition name="fade-transform" mode="out-in">
-				<keep-alive :include="cachedViews">
-					<router-view class="views" :key="key"/>
-				</keep-alive>
-			</transition>
-			<!-- 底部 -->
-			<p class="footer">Copyright © {{year}} {{$t('base.name')}} All Rights Reserved.&nbsp;&nbsp;&nbsp;Email: 329255122@qq.com</p>
-		</div>
-		<!-- 固定右侧工具 -->
-		<fixedTools/>
-	</div>
+    <div class="wrap" :class="{'collapse-wrap':isCollapse,'isFixed':menuFixed,'menuType-2':menuType === 2,'content-width-type-2':contentWidthType==='Fixed'}">
+        <!-- 顶部信息条 -->
+        <topBar/>
+        <!-- 侧边菜单 -->
+        <sideBar/>
+        <!-- 右-部分 -->
+        <div class="right-wrap">
+            <!-- 面包屑 -->
+            <crumb/>
+            <!-- 多标签 -->
+            <tags-view/>
+            <!-- 页面容器 -->
+            <transition name="fade-transform" mode="out-in">
+                <!-- <keep-alive :include="cachedViews"> -->
+                <router-view class="views" :key="key"/>
+                <!-- </keep-alive> -->
+            </transition>
+            <!-- 底部 -->
+            <p class="footer">Copyright © {{year}} {{$t('base.name')}} All Rights Reserved.&nbsp;&nbsp;&nbsp;Email: 329255122@qq.com</p>
+        </div>
+        <!-- 固定右侧工具 -->
+        <fixedTools/>
+    </div>
 </template>
 <script>
 import sideBar from "components/sideBar"; //侧边导航
@@ -31,119 +31,119 @@ import TagsView from "components/TagsView"; //标签页
 import fixedTools from "components/siteSetting"; //页面设置
 
 export default {
-	name: "index",
-	data() {
-		return {
-			instance: null,
-			year: new Date().getFullYear(),
-			isZoomOpen: false
-		};
-	},
-	created() {
-		this.$store.dispatch("getCountry");
-		this.$store.dispatch("getNation");
-		this.$store.dispatch("getCities");
-	},
-	computed: {
-		cachedViews() {
-			return this.$store.state.tagsView.cachedViews;
-		},
-		isCollapse() {
-			return this.$store.state.menuCollapse;
-		},
-		menuFixed() {
-			return this.$store.state.menuFixed;
-		},
-		menuType() {
-			return this.$store.state.menuType;
-		},
-		contentWidthType() {
-			return this.$store.state.contentWidthType;
-		},
-		key() {
-			return this.$route.fullPath;
-		}
-	},
-	components: {
-		sideBar,
-		topBar,
-		crumb,
-		TagsView,
-		fixedTools
-	},
-	mounted() {
-		const that = this;
-		this.isZoom();
-		var timer = null;
-		window.addEventListener("resize", function() {
-			// 防抖处理
-			if (timer) {
-				clearTimeout(timer);
-			}
-			timer = setTimeout(function() {
-				that.isZoom();
-			}, 200);
-		});
-	},
-	methods: {
-		// 浏览器缩放提示 （打开控制台会影响准确度）
-		detectZoom: function() {
-			let ratio = 0,
-				screen = window.screen,
-				ua = navigator.userAgent.toLowerCase();
-			//
-			if (~ua.indexOf("firefox")) {
-				if (window.devicePixelRatio !== undefined) {
-					ratio = window.devicePixelRatio;
-				}
-			} else if (~ua.indexOf("msie")) {
-				if (screen.deviceXDPI && screen.logicalXDPI) {
-					ratio = screen.deviceXDPI / screen.logicalXDPI;
-				}
-			} else if (
-				window.outerWidth !== undefined &&
-				window.innerWidth !== undefined
-			) {
-				ratio = window.outerWidth / window.innerWidth;
-			}
-			//
-			if (ratio) {
-				ratio = Math.round(ratio * 100);
-			}
-			// 360安全浏览器下的innerWidth包含了侧边栏的宽度
-			if (ratio !== 100) {
-				if (ratio >= 95 && ratio <= 105) {
-					ratio = 100;
-				}
-			}
-			return ratio;
-		},
-		isZoom: function() {
-			if (this.detectZoom() < 100 || this.detectZoom() > 100) {
-				this.zoomNotifyOpen();
-			} else {
-				this.zoomNotifyClose();
-			}
-		},
-		zoomNotifyOpen: function() {
-			if (!this.isZoomOpen) {
-				this.isZoomOpen = true; // 加上标记防止多次提示
-				this.instance = this.$notify({
-					title: this.$t("base.tips"),
-					message: this.$t("base.zoom"),
-					type: "info",
-					duration: 0
-				});
-			}
-		},
-		zoomNotifyClose: function() {
-			if (this.instance !== null) {
-				// 判断是否为null 防止bug
-				this.instance.close();
-				this.isZoomOpen = false;
-			}
-		}
-	}
+    name: "index",
+    data() {
+        return {
+            instance: null,
+            year: new Date().getFullYear(),
+            isZoomOpen: false
+        };
+    },
+    created() {
+        this.$store.dispatch("getCountry");
+        this.$store.dispatch("getNation");
+        this.$store.dispatch("getCities");
+    },
+    computed: {
+        cachedViews() {
+            return this.$store.state.tagsView.cachedViews;
+        },
+        isCollapse() {
+            return this.$store.state.menuCollapse;
+        },
+        menuFixed() {
+            return this.$store.state.menuFixed;
+        },
+        menuType() {
+            return this.$store.state.menuType;
+        },
+        contentWidthType() {
+            return this.$store.state.contentWidthType;
+        },
+        key() {
+            return this.$route.fullPath;
+        }
+    },
+    components: {
+        sideBar,
+        topBar,
+        crumb,
+        TagsView,
+        fixedTools
+    },
+    mounted() {
+        const that = this;
+        this.isZoom();
+        var timer = null;
+        window.addEventListener("resize", function() {
+            // 防抖处理
+            if (timer) {
+                clearTimeout(timer);
+            }
+            timer = setTimeout(function() {
+                that.isZoom();
+            }, 200);
+        });
+    },
+    methods: {
+        // 浏览器缩放提示 （打开控制台会影响准确度）
+        detectZoom: function() {
+            let ratio = 0,
+                screen = window.screen,
+                ua = navigator.userAgent.toLowerCase();
+            //
+            if (~ua.indexOf("firefox")) {
+                if (window.devicePixelRatio !== undefined) {
+                    ratio = window.devicePixelRatio;
+                }
+            } else if (~ua.indexOf("msie")) {
+                if (screen.deviceXDPI && screen.logicalXDPI) {
+                    ratio = screen.deviceXDPI / screen.logicalXDPI;
+                }
+            } else if (
+                window.outerWidth !== undefined &&
+                window.innerWidth !== undefined
+            ) {
+                ratio = window.outerWidth / window.innerWidth;
+            }
+            //
+            if (ratio) {
+                ratio = Math.round(ratio * 100);
+            }
+            // 360安全浏览器下的innerWidth包含了侧边栏的宽度
+            if (ratio !== 100) {
+                if (ratio >= 95 && ratio <= 105) {
+                    ratio = 100;
+                }
+            }
+            return ratio;
+        },
+        isZoom: function() {
+            if (this.detectZoom() < 100 || this.detectZoom() > 100) {
+                this.zoomNotifyOpen();
+            } else {
+                this.zoomNotifyClose();
+            }
+        },
+        zoomNotifyOpen: function() {
+            if (!this.isZoomOpen) {
+                this.isZoomOpen = true; // 加上标记防止多次提示
+                this.instance = this.$notify({
+                    title: this.$t("base.tips"),
+                    message: this.$t("base.zoom"),
+                    type: "info",
+                    duration: 0
+                });
+            }
+        },
+        zoomNotifyClose: function() {
+            if (this.instance !== null) {
+                // 判断是否为null 防止bug
+                this.instance.close();
+                this.isZoomOpen = false;
+            }
+        }
+    }
 };
 </script>
 
@@ -151,26 +151,26 @@ export default {
 /*fade*/
 .fade-enter-active,
 .fade-leave-active {
-	transition: opacity 0.28s;
+    transition: opacity 0.28s;
 }
 
 .fade-enter,
 .fade-leave-active {
-	opacity: 0;
+    opacity: 0;
 }
 
 /*fade-transform*/
 .fade-transform-leave-active,
 .fade-transform-enter-active {
-	transition: all 0.5s;
+    transition: all 0.5s;
 }
 .fade-transform-enter {
-	opacity: 0;
-	transform: translateX(-30px);
+    opacity: 0;
+    transform: translateX(-30px);
 }
 .fade-transform-leave-to {
-	opacity: 0;
-	transform: translateX(30px);
+    opacity: 0;
+    transform: translateX(30px);
 }
 </style>
 
